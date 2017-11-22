f7f260d1a5dd79d23b33463b7400e846b6f5a81b
admin <chaoyue.liu@chinaredstar.com>
2017-11-22 09:37:08 +0800

    初始化

---
+++ CYFM/CYFM/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  CYFM
+//
+//  Created by admin on 2017/11/22.
+//  Copyright © 2017年 admin. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
---
+++ CYFM/CYFM/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  CYFM
+//
+//  Created by admin on 2017/11/22.
+//  Copyright © 2017年 admin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
